9c8460d7c7d3be2c01e94607453915c1e3f0116c
Andrew AKre <ajjagecko@gmail.com>
2024-10-07 13:44:33 -0400

    Added sync methods to package lib, init Lab 5 commit

--- package/package_lib/cpet_343_components.vhd
+++ package/package_lib/cpet_343_components.vhd
@@ -55,4 +55,25 @@ package cpet_343_components is
          two_comp_o     :out std_logic_vector(bits-1 downto 0)
       );
    end component;
+   ------ Rising Edge Synchronizer ------
+   component rising_edge_synchronizer is
+      port (
+         clk               : in std_logic;
+         reset             : in std_logic;
+         input             : in std_logic;
+         edge              : out std_logic
+      );
+   end component;
+   ------ Generic Synchronizer ------
+   component generic_sync_arch is
+      generic (
+         bits    : integer := 4
+      );   
+      port (
+         clk               : in std_logic;
+         reset             : in std_logic;
+         async_i           : in std_logic_vector(bits-1 downto 0);
+         sync_o            : out std_logic_vector(bits-1 downto 0)
+      );
+   end component;
 end package cpet_343_components;
